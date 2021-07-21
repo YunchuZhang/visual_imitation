f0c12ee49321867a91db8ab7061f1b1f4ff1304c
yunchuzhang <yunchu@g.ucla.edu>
2021-07-21 18:02:43 -0400

    nope

--- models/builder.py
+++ models/builder.py
@@ -28,22 +28,24 @@ class Featurenet(nn.Module):
 		return x
 
 class PositionalEncoding(nn.Module):
+	'''
+	position encoding with sin and cos
+	'''
+	def __init__(self, d_model, dropout=0.1, max_len=5000):
+		super(PositionalEncoding, self).__init__()
+		self.dropout = nn.Dropout(p=dropout)
 
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
+		pe = torch.zeros(max_len, d_model)
+		position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+		div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+		pe[:, 0::2] = torch.sin(position * div_term)
+		pe[:, 1::2] = torch.cos(position * div_term)
+		pe = pe.unsqueeze(0).transpose(0, 1)
+		self.register_buffer('pe', pe)
 
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
+	def forward(self, x):
+		x = x + self.pe[:x.size(0), :]
+		return self.dropout(x)
 
 class SeqGoalBC(nn.Module):
 	'''
@@ -56,29 +58,42 @@ class SeqGoalBC(nn.Module):
 		self.n_head = params['n_head']
 		self.n_layers = params['n_layers']
 
-		# sequential network
+		# sequential network (transfermer)
+		# https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 		encoder_layer = nn.TransformerEncoderLayer(d_model=self.feat_dim, n_head = 8)
 		self.transformer_encoder = nn.TransformerEncoder(encoder_layer, self.n_layers)
 		self.pos_encoder = PositionalEncoding(self.feat_dim, dropout = 0.1)
 
-		"""
-		to do 
-		"""
+		# 
+
 		resnet = models.resnet18(pretrained = pretrained)
 		self.encoder = nn.Sequential(*list(resnet.children())[0:9])
 
 		self.fc1 = nn.Linear(512, feat_dim)
+		self.fc1 = nn.Linear(512, feat_dim)
 
-	def forward(self, cur_img, goal_img):
-		x = self.fc1(self.encoder(cur_img))
-		#x = self.pool(F.relu(self.conv2(x)))
-		x = x.view(-1,256)
-		goal = self.fc1(self.encoder(goal))
-		#x = self.pool(F.relu(self.conv2(x)))
-		goal = goal.view(-1,256)
-		return 
+	def forward(self, feat, gripper = False):
+		'''
+        Args:
+        	feat : (B, S, E) 16,3,512
+        	gripper : whether predict gripper 
+        Output:
+            translation : (B, 3)
+            rotation : (B, 6)
+            gripper : (B, 1)
+        '''
+		B, S, E = feat.shape
 
+		feat = feat.reshape(B, S, self.feat_dim).permute(1,0,2).float() # (S, B, E)
+		feat = self.pos_encoder(feat)
+		feat = self.transformer_encoder(feat)
+        cnn_emb = feat.clone()
 
+# x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+		# gripper net
+		if gripper == True:
+			F.relu(self.conv1(x))
+			x = x.view(-1,512)
 
 		# POSITION NET
 		pfc1 = nn.Linear(in_features=self.dimm, out_features=self.feat_dim, bias=True).to(self.device) # *3 for 3 images
@@ -116,21 +131,24 @@ class SeqGoalBC(nn.Module):
 		dep_a3dfc2 = nn.Linear(in_features=self.feat_dim, out_features=3, bias=True).to(self.device)  # 1 * 3 matrix
 		self.dep_a3d_net = nn.Sequential(dep_a3dfc1, nn.ReLU(), dep_a3dfc2).to(self.device).to(self.device)
 
-    def __init__(self, feat_dim = 2048, pretrained = True):
-        super(Conditional_Net_RN5N, self).__init__()
-        resnet = models.resnet18(pretrained = pretrained)
-        self.resnet_l5 = nn.Sequential(*list(resnet.children())[0:8])
-        self.conv1 = nn.Conv2d(512, 512, kernel_size = 3, padding = 1, bias = True)
-        #self.conv2 = nn.Conv2d(512, 512, kernel_size = 3, padding = 1, bias = True)
-        self.fc1 = nn.Linear(512, feat_dim)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.bn1 = nn.BatchNorm2d(512)
-        self.bn2 = nn.BatchNorm2d(512)
-            
-    def forward(self, x):
-        x=self.pool(self.resnet_l5(x))
-        x = self.pool(F.relu(self.conv1(x)))
-        #x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1,512)
-        x = self.fc1(x)
-        return x
+	def __init__(self, feat_dim = 2048, pretrained = True):
+		super(Conditional_Net_RN5N, self).__init__()
+		resnet = models.resnet18(pretrained = pretrained)
+		self.resnet_l5 = nn.Sequential(*list(resnet.children())[0:8])
+		self.conv1 = nn.Conv2d(512, 512, kernel_size = 3, padding = 1, bias = True)
+		#self.conv2 = nn.Conv2d(512, 512, kernel_size = 3, padding = 1, bias = True)
+		self.fc1 = nn.Linear(512, feat_dim)
+		self.pool = nn.MaxPool2d(2, 2)
+		self.bn1 = nn.BatchNorm2d(512)
+		self.bn2 = nn.BatchNorm2d(512)
+			
+	def forward(self, x):
+		x=self.pool(self.resnet_l5(x))
+		x = self.pool(F.relu(self.conv1(x)))
+		#x = self.pool(F.relu(self.conv2(x)))
+		x = x.view(-1,512)
+		x = self.fc1(x)
+		return x
+if __name__ == '__main__':
+	src = torch.rand(10, 32, 512)
+	# transformer_encoder(src) (10,32,512)
